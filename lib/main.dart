f247bd470aad82193cb8e99ff8f85acb279be6cd
Soikat77 <soikatalam26@gmail.com>
2021-08-17 19:04:03 +0600

    return MaterialApp

--- lib/main.dart
+++ lib/main.dart
@@ -3,15 +3,16 @@ import 'package:flutter/material.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Expence Tracker'),
-      ),
-      body: Center(
-        child: Text('All yor expences will show here'),
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Expence Tracker'),
+        ),
+        body: Center(
+          child: Text('All yor expences will show here'),
+        ),
       ),
     );
   }
