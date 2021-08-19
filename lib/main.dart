5133939c1cbd04f815b0e8428acce2738249506d
Soikat77 <soikatalam26@gmail.com>
2021-08-19 19:49:15 +0600

    ui improvement

--- lib/main.dart
+++ lib/main.dart
@@ -19,15 +19,15 @@ class MyHomePage extends StatelessWidget {
     Expences(
       id: 'exp1',
       title: 'Bying a T-Shirt',
-      ammount: 200.00,
+      ammount: 200.75,
       date: DateTime.now(),
     ),
     Expences(
       id: 'exp2',
       title: 'Going to Dinner',
-      ammount: 175.50,
+      ammount: 175.99,
       date: DateTime.now(),
-    )
+    ),
   ];
 
   @override
@@ -51,53 +51,53 @@ class MyHomePage extends StatelessWidget {
           Column(
             children: expences.map((exp) {
               return Card(
-                  color: Colors.blue[50],
-                  elevation: 3,
-                  margin: EdgeInsets.all(10),
-                  child: Row(
-                    children: [
-                      Container(
-                        margin: EdgeInsets.symmetric(
-                          vertical: 10,
-                          horizontal: 15,
-                        ),
-                        padding: EdgeInsets.all(10),
-                        child: Text(
-                          exp.ammount.toString(),
+                color: Colors.blue[50],
+                elevation: 3,
+                margin: EdgeInsets.all(10),
+                child: Row(
+                  children: [
+                    Container(
+                      margin: EdgeInsets.all(10),
+                      height: 50,
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            exp.title!,
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 18,
+                              color: Colors.blue,
+                            ),
+                          ),
+                          Text(
+                            exp.date.toString(),
+                            style: TextStyle(
+                              fontWeight: FontWeight.normal,
+                              fontSize: 14,
+                              color: Colors.blue[400],
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.end,
+                      children: [
+                        Text(
+                          '\$ ${exp.ammount.toString()}',
                           style: TextStyle(
                             fontWeight: FontWeight.bold,
-                            fontSize: 24,
+                            fontSize: 22,
                             color: Colors.blue,
                           ),
                         ),
-                      ),
-                      Container(
-                        height: 50,
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.spaceAround,
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text(
-                              exp.title!,
-                              style: TextStyle(
-                                fontWeight: FontWeight.bold,
-                                fontSize: 18,
-                                color: Colors.blue,
-                              ),
-                            ),
-                            Text(
-                              exp.date.toString(),
-                              style: TextStyle(
-                                fontWeight: FontWeight.normal,
-                                fontSize: 14,
-                                color: Colors.blue[400],
-                              ),
-                            ),
-                          ],
-                        ),
-                      )
-                    ],
-                  ));
+                      ],
+                    ),
+                  ],
+                ),
+              );
             }).toList(),
           ),
         ],
